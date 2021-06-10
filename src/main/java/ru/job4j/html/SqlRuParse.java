f431c331db7d02ea8eeac3e0a5f53d3da89638e0
stGOST <dda.90@mail.ru>
2021-06-10 19:49:46 +0500

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -6,14 +6,21 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class SqlRuParse {
+    private static String url = "https://www.sql.ru/forum/job-offers";
+
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            System.out.println(td.parent().child(5).text());
+        for (int i = 1; i <= 5; i++) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(url).append("/").append(i);
+            System.out.println(stringBuilder);
+            Document doc = Jsoup.connect(stringBuilder.toString()).get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                Element href = td.child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                System.out.println(td.parent().child(5).text());
+            }
         }
     }
 }
