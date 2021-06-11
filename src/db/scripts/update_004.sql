65fb837226f2a3d7bb83f1ce473099425df39108
stGOST <dda.90@mail.ru>
2021-06-11 15:09:53 +0500

    4. Схема таблицы Post [#1731 #166602]

---
+++ src/db/scripts/update_004.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS rabbit (
+	rabbit_id serial PRIMARY KEY,
+	created_date timestamp
+);
+
+CREATE TABLE IF NOT EXISTS posts (
+post_id serial PRIMARY KEY,
+name varchar(1024) NOT NULL,
+text text NOT NULL,
+link varchar(1024) NOT NULL,
+date timestamp NOT NULL,
+unique(link)
+);
