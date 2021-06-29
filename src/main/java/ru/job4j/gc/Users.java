a7adc53c2e44d4042f9584c9cd7ddfa30fbf3296
stGOST <dda.90@mail.ru>
2021-06-29 19:23:56 +0500

    JMH

--- src/main/java/ru/job4j/gc/Users.java
+++ src/main/java/ru/job4j/gc/Users.java
@@ -1,14 +1,37 @@
 package ru.job4j.gc;
 
-public class Users {
+import org.openjdk.jmh.annotations.*;
 
-    private final int id;
-    private final String name;
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Benchmark)
+public class Users {
+    @Param({ "100" })
+    private int id;
+    private String name = null;
     private final int test = 10;
 
     public Users(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public Users() {
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.All) // тестируем во всех режимах
+    @Warmup(iterations = 1) // число итераций для прогрева нашей функции
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    @Measurement(iterations = 2, batchSize = 2)
+    public void measureName() {
+        setTest();
+    }
+
+    public void setTest() {
+        for (int i = 0; i < 100_000; i++) {
+            id++;
+        }
     }
 
     @Override
